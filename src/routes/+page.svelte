81c7eaa3c35dd7a369b55bfc669c9a329e0c2347
DuvalEnzo <enzo.duval94@live.fr>
2024-10-24 15:53:28 +0200

    Création d'un fichier layout implémentation de la navbar dans celui-ci

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,29 +3,12 @@
 </head>
 
 <script>
-    import photoEnzo from '$lib/images/image_accueil_navbar.png';
     import iconGithub from '$lib/images/logo_github.svg';
     import iconLinkedin from '$lib/images/logo_linkedin.svg';
     import iconMail from '$lib/images/logo_mail.svg';
-</script>
+
+    </script>
 <body>
-<nav>
-    <img class="photoEnzo" src={photoEnzo} alt="">
-    <ul>
-        <li>
-            <a href="/">Accueil</a>
-        </li>
-        <li>
-            <a href="/Experiences">Expériences</a>
-        </li>
-        <li>
-            <a href="/Projets">Mes Projets</a>
-        </li>
-        <li>
-            <a href="/Contact" class="button">Contact</a>
-        </li>
-    </ul>
-</nav>
 <div class="Presentation">
     <h2 class="presentationh2">Bonjour, je suis</h2>
     <h1 class="presentationh1">Enzo Duval</h1>
@@ -47,41 +30,6 @@
 <style>
     body {
         font-family: Montserrat, sans-serif;
-    }
-
-    ul {
-        display: flex;
-        justify-content: flex-end;
-        margin-right: 100px;
-        padding: 0;
-        list-style: none;
-    }
-
-    li {
-        margin-left: 64px;
-    }
-
-    a {
-        text-decoration: none;
-        color: #FFFFFF;
-        font-weight: 700;
-        transition: color 0.3s;
-    }
-
-    a:hover {
-        color: #002333;
-    }
-
-    button {
-        background-color: #002333;
-    }
-
-    .photoEnzo {
-        right: 0;
-        top: 0;
-        z-index: -1;
-        position: absolute;
-        height: 110%;
     }
 
     .Presentation {
@@ -114,15 +62,11 @@
         margin-left: 22%;
         margin-top: 2%;
     }
-
-  
     .icon img {
         width: 40px;
         height: 40px;
         transition: transform 0.2s ease;
     }
-
-
     .icon img:hover {
         transform: scale(1.2);
     }
